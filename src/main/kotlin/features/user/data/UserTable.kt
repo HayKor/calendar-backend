14c176bbd76b034a81cfe2362deae309ef068fa2
Arthur <baginian.artur@yandex.ru>
2026-03-21 18:34:04 +0300

    feat(user): change user table a little

--- src/main/kotlin/features/user/data/UserTable.kt
+++ src/main/kotlin/features/user/data/UserTable.kt
@@ -9,7 +9,8 @@ import org.jetbrains.exposed.sql.kotlin.datetime.datetime
 object UserTable : IntIdTable("users") {
     val username = varchar("username", 50).uniqueIndex()
     val email = varchar("email", 100).uniqueIndex()
-    val hashedPassword = varchar("hashed_password", 255)
+    val hashedPassword = varchar("hashed_password", 255).nullable()
+    val isVerified = bool("isVerified").nullable()
     val createdAt = datetime("created_at").clientDefault {
         Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
     }
